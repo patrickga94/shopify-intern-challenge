02ccc9321b3925eadb1650ac0f81b797d2c6770f
patrickga <patrick.g.alvarez@gmail.com>
2022-05-20 14:28:45 -0500

    Adds an input form where a user can input their request to the api

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,20 +1,47 @@
 import React, {useState, useEffect} from "react";
 import axios from 'axios';
 import {makeRequest} from '../api/AiRequest'
+import {Form, Button, Container} from "react-bootstrap"
 require('dotenv').config()
 const apiKey = process.env.REACT_APP_AI_APIKEY
 
 const InputForm = (props) => {
-    useEffect(() =>{
-        makeRequest("Write a poem about a dog wearing skis", apiKey)
+    const [input, setInput] = useState(null)
+    const [responses, setResponses] = useState([])
+
+
+    const handleChange= (e) => {
+        e.persist()
+        setInput(e.target.value)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log("input", input)
+        makeRequest(input, apiKey)
             .then(res => {
+                console.log("full res", res.data)
                 console.log("response", res.data.choices[0].text)
             })
             .catch(console.error)
-    }, [])
+    }
+
+    // useEffect(() =>{
+    //     makeRequest(input, apiKey)
+    //         .then(res => {
+    //             console.log("full res", res.data)
+    //             console.log("response", res.data.choices[0].text)
+    //         })
+    //         .catch(console.error)
+    // }, [input])
     return (
         <>
             <h2>Hello there</h2>
+            <Container>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Control name="request" type="text" onChange={handleChange}/>
+                    <Button className="mt-2" type="submit">Submit</Button>
+                </Form>
+            </Container>
         </>
     )
 }
